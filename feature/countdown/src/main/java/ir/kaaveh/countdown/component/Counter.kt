e115137c2d936d95ef11a3dead43df791ddb485d
Kaaveh Mohamedi <Kaaveh@pm.me>
2023-06-14 07:25:46 +0330

    [feature] add Counter widget

---
+++ feature/countdown/src/main/java/ir/kaaveh/countdown/component/Counter.kt
@@ -0,0 +1,35 @@
+package ir.kaaveh.countdown.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import ir.kaaveh.designesystem.ThemePreviews
+import ir.kaaveh.designesystem.theme.ComposeBreakTheme
+
+@Composable
+fun Counter(
+    minutes: String,
+    seconds: String,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        CounterText(text = minutes)
+
+        Text(text = " : ")
+
+        CounterText(text = seconds)
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun CounterPreview() {
+    ComposeBreakTheme {
+        Surface {
+            Counter(minutes = "19", seconds = "39")
+        }
+    }
+}
