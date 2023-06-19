7def75598f12c6dee9a0a9fc60406fa74bd50c11
Kaaveh Mohamedi <Kaaveh@pm.me>
2023-06-19 07:16:41 +0330

    [optimize] change stop icon

--- library/designsystem/build.gradle.kts
+++ library/designsystem/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     api("androidx.compose.ui:ui-graphics")
     api("androidx.compose.ui:ui-tooling-preview")
     api("androidx.compose.material3:material3")
+    api("androidx.compose.material:material-icons-extended:1.5.0-beta02")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
